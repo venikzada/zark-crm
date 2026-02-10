f9656bd0580c600c033af713d56a423bc8b6c987
Renan Kennedy <renankennedy@MacBook-Air-de-Renan.local>
2026-02-10 08:28:17 -0300

    Versão 0.1.34 - Fix Image Config (SVG + GitHub + Project Domain)

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,21 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'wvzevafrabddtbexgijp.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
     ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
